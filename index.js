c120bcf5b2da7518114c40c4db6a6383058241c3
Nicolae Levinta <nicolae.levintsa@gmail.com>
2022-11-06 14:56:55 +0200

    added resume

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-console.log('Hi!');
+const port = 4000;
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.use(express.static("."));
+
+app.get('/',(req,res)=>{
+    fs.readFile('./views/home.html',(err,data)=>{
+        if(err){
+            console.log(err)
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+});
+app.get('/form',(req,res)=>{
+    fs.readFile('./views/form.html',(err,data)=>{
+        if(err){
+            console.log(err)
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+});
+app.get('/views/results.html',(req,res)=>{
+    fs.readFile('./views/results.html',(err,data)=>{
+        if(err){
+            console.log(err)
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+});
+
+app.get('/resume',(req,res)=>{
+    fs.readFile('./views/myresume.html',(err,data)=>{
+        if(err){
+            console.log(err)
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+});
+
+app.listen(port, ()=>{
+    console.log('Server started')
+    })
+        
